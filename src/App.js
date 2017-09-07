71508cf1b079c85b6f8a7036640a33fd6e2946ff
Hazem <hazem@tamhub.com>
2017-09-07 19:49:55 +0300

    Add popup modal to add new product

--- src/App.js
+++ src/App.js
@@ -3,11 +3,55 @@ import './App.css';
 import './table.js';
 import '../node_modules/font-awesome/css/font-awesome.min.css'; 
 
+export class Modal extends React.Component {
+  render() {
+    if (this.props.isOpen === false)
+      return null
 
-class Products extends React.Component {
+    let modalStyle = {
+      position: 'absolute',
+      top: '50%',
+      left: '50%',
+      transform: 'translate(-50%, -50%)',
+      zIndex: '9999',
+      background: '#FFFFFF',
+      width: '400px',
+      height: '500px',
+      padding: '10px'
+    }
+
+    let backdropStyle = {
+      position: 'absolute',
+      width: '100%',
+      height: '100%',
+      top: '0px',
+      left: '0px',
+      zIndex: '9998',
+      background: 'rgba(0, 0, 0, 0.3)'
+    }
+
+    return (
+      <div>
+        <div style={modalStyle}>{this.props.children}</div>
+        <div style={backdropStyle} onClick={e => this.close(e)}/>}
+      </div>
+    )
+    
+  }
+
+  close(e) {
+    e.preventDefault()
+
+    if (this.props.onClose) {
+      this.props.onClose()
+    }
+  }
+}
+
+export default class Products extends React.Component {
     constructor() {
         super();
-        this.state = { products: [] };
+        this.state = { products: [], isModalOpen: false };
 
     }
     
@@ -23,6 +67,14 @@ class Products extends React.Component {
                 this.setState({products: json });
             });
     }
+    
+    openModal() {
+    this.setState({ isModalOpen: true })
+  }
+
+  closeModal() {
+    this.setState({ isModalOpen: false })
+  }
     
     render() {        
         return(
@@ -65,9 +117,28 @@ class Products extends React.Component {
               </tbody>
               </table>
             </div>
-            <button type="button" className="btn-class" onClick={this.handleClick.bind(this)}>Add New Product</button>
+            
+            <div>
+              <button type="button" className="btn-class" onClick={() => this.openModal()}>Add New Product</button>
+              <Modal isOpen={this.state.isModalOpen} onClose={() => this.closeModal()}>
+                <h2>Add New Product</h2>
+                <p>Product name</p>
+                <input type="text" id="Name"/> 
+                <p>Product serial number</p>
+                <input type="text" id="serialNumber"/>
+                <p>Product quantity</p>
+                <input type="text" id="quantity"/>
+                <p>Product price</p>
+                <input type="text" id="price"/>
+                <p>Product date added</p>
+                <input type="date" id="dateAdded"/> <br />
+                <button type="button" className="btn-class">Add</button>
+                
+              </Modal>
             </div>
+
+            </div>
+            
         );
     }
 }
-export default Products;
