c3d9e0b79bab804f6a2ca1b64fc58b875841c513
Hazem <hazem@tamhub.com>
2017-09-08 00:09:42 +0300

    Modify add product function with refs

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import './App.css';
 import './table.js';
 import '../node_modules/font-awesome/css/font-awesome.min.css'; 
+import $ from 'jquery'; 
+
 
 export class Modal extends React.Component {
   render() {
@@ -52,13 +54,31 @@ export default class Products extends React.Component {
     constructor() {
         super();
         this.state = { products: [], isModalOpen: false };
+        this.addProduct = this.addProduct.bind(this);
 
+
+    }
+
+    addProduct(e) {
+      
+      const name = (this.refs.name).value;
+      const serialNumber = (this.refs.serialNumber).value;
+      const quantity = (this.refs.quantity).value;
+      const price = (this.refs.price).value;
+      const dateAdded = (this.refs.dateAdded).value;
+      
+      const product = {name, serialNumber, quantity, price, dateAdded};
+      
+      e.preventDefault();
+      $.post('http://localhost:5000/products', product) 
+      alert("Product " + name + " has been added");
+        
     }
     
     handleClick(event) {
-    event.preventDefault();
-    console.log("Hazem");
-  }
+      event.preventDefault();
+      console.log("Hazem");
+    }
     
     componentDidMount() {
         fetch('http://localhost:5000/products')
@@ -119,22 +139,24 @@ export default class Products extends React.Component {
             </div>
             
             <div>
+            <form onSubmit={this.addProduct}>
               <button type="button" className="btn-class" onClick={() => this.openModal()}>Add New Product</button>
               <Modal isOpen={this.state.isModalOpen} onClose={() => this.closeModal()}>
                 <h2>Add New Product</h2>
                 <p>Product name</p>
-                <input type="text" id="Name"/> 
+                <input type="text" id="name" ref="name" /> 
                 <p>Product serial number</p>
-                <input type="text" id="serialNumber"/>
+                <input type="text" id="serialNumber" ref="serialNumber"/>
                 <p>Product quantity</p>
-                <input type="text" id="quantity"/>
+                <input type="text" id="quantity" ref="quantity"/>
                 <p>Product price</p>
-                <input type="text" id="price"/>
+                <input type="text" id="price" ref="price"/>
                 <p>Product date added</p>
-                <input type="date" id="dateAdded"/> <br />
-                <button type="button" className="btn-class">Add</button>
+                <input type="date" id="dateAdded" ref="dateAdded"/> <br />
+                <button type="submit" className="btn-class">Add</button>
                 
               </Modal>
+              </form>
             </div>
 
             </div>
