4000a0df7827a03b47d6ad1381f91622a6a7b96e
Hazem <hazem@tamhub.com>
2017-09-12 09:33:41 +0300

    Add localization to project

--- src/App.js
+++ src/App.js
@@ -6,6 +6,60 @@ import '../node_modules/sweetalert2/dist/sweetalert2.css';
 import swal from '../node_modules/sweetalert2/dist/sweetalert2.js'; 
 import $ from 'jquery';
 import Modal from './Modal.js';
+import LocalizedStrings from 'react-localization';
+
+let strings = new LocalizedStrings({
+ en:{
+   product: "Product",
+   language: "العربية",
+   productId: "Product Id",
+   productName: "Product Name",
+   serialNumber: "Serial Number",
+   quantity: "Quantity",
+   price: "Price",
+   dateAdded: "Date Added",
+   action: "Action",
+   deleteAction: "Delete",
+   addAction: "Add New Product",
+   add: "Add",
+   successAdd: "Product successfully added",
+   successHeader: "Success",
+   failedAdd: "Adding product failed",
+   errorHeader: "Oops...",
+   deleteConfirmationTitle: "Are you sure? you want to delete the product",
+   deleteConfirmationBody: "You won't be able to revert this!",
+   confirmButtonText: "Yes, delete it!",
+   deleted: "Deleted!",
+   deletedMsg: "Product has been deleted.",
+   failedDelet: "Deleting product failed"
+ },
+ ar: {
+   product:"المنتجات",
+   language: "English",
+   productId: "رقم المنتج",
+   productName: "اسم النتج",
+   serialNumber: "الرقم التسلسلي",
+   quantity: "كمية",
+   price: "السعر",
+   dateAdded: "تاريخ الاضافة",
+   action: "العمليات",
+   deleteAction: "حذف",
+   addAction: "اضافة منتج",
+   add: "اضافة",
+   successAdd: "تمت اضافة المنتج بنجاح",
+   successHeader: "تم",
+   failedAdd: "فشل إضافة المنتج",
+   errorHeader: "عفوا ...",
+   deleteConfirmationTitle: "هل أنت واثق؟ تريد حذف المنتج",
+   deleteConfirmationBody: "!لن تكون قادرة على اعادة هذا المنتج",
+   confirmButtonText: "نعم، احذفه!",
+   deleted: "!تم الحذف",
+   deletedMsg: ".تم حذف المنتج",
+   failedDelet: "أخفق حذف المنتج"
+ }
+});
+
+let divDirection = "ltr";
 
 export default class Products extends React.Component {
     constructor() {
@@ -14,7 +68,18 @@ export default class Products extends React.Component {
         this.handleProduct = this.handleProduct.bind(this);
 
     }
-
+    switchLanguage() {
+      if (strings.getLanguage() === 'en') {
+        strings.setLanguage('ar');
+        divDirection = "rtl"
+      }
+      else if (strings.getLanguage() === 'ar') {
+        strings.setLanguage('en');
+        divDirection = "ltr"
+      }
+      this.setState({})
+    }
+    
     handleProduct(e) {
       
       const name = (this.refs.name).value;
@@ -26,19 +91,17 @@ export default class Products extends React.Component {
       const product = {name, serialNumber, quantity, price, dateAdded};
       this.closeModal();
       e.preventDefault();
-      $.post('http://localhost:5000/products', product).then((response) => {
-          
+      $.post('http://localhost:5000/products', product).then((response) => {    
           swal(
-            "Success",
-            "Product " + name + " has been added",
+            strings.successHeader,
+            strings.successAdd,
             "success"
           );
-          console.log(response);
       })
       .catch((error) => {
         swal(
-          "Oops...", 
-          "Adding product failed", 
+          strings.errorHeader, 
+          strings.failedAdd, 
           "error"
         );
         console.log(error);
@@ -48,24 +111,24 @@ export default class Products extends React.Component {
     handleDelete(id) {
       
       swal({
-        title: "Are you sure? you want to delete product number: " + id,
-        text: "You won't be able to revert this!",
+        title: strings.deleteConfirmationTitle,
+        text: strings.deleteConfirmationBody,
         type: 'warning',
         showCancelButton: true,
         confirmButtonColor: '#3085d6',
         cancelButtonColor: '#d33',
-        confirmButtonText: 'Yes, delete it!'
+        confirmButtonText: strings.confirmButtonText
       }).then(function () {
         $.ajax({
         url: 'http://localhost:5000/products/' + id,
         type: 'DELETE',
-        success: function (){swal('Deleted!', 'Product has been deleted.', 'success')},
-        error: function (){swal("Oops...", "Deleting product failed",  "error")}
+        success: function (){swal(strings.deleted, strings.deletedMsg, 'success')},
+        error: function (){swal(strings.errorHeader, strings.failedDelet,  "error")}
       });
     });
-  }
-    
-  componentDidMount() {
+    }
+  
+    componentDidMount() {
         fetch('http://localhost:5000/products')
             .then(response => response.json())
             .then(json=> {
@@ -73,29 +136,30 @@ export default class Products extends React.Component {
             });
     }
     
-  openModal() {
-  this.setState({ isModalOpen: true })
-  }
+    openModal() {
+    this.setState({ isModalOpen: true })
+    }
 
-  closeModal() {
-    this.setState({ isModalOpen: false })
-  }
+    closeModal() {
+      this.setState({ isModalOpen: false })
+    }
     
-    render() {        
+      render() {        
         return(
-          <div>
-          <h1>Products</h1>
+          <div dir={divDirection}>
+          <input type="button" onClick={() => this.switchLanguage()} value={strings.language} className="btn-class"/> 
+          <h1>{strings.product}</h1>
            <div className="tbl-header">
               <table cellPadding="0" cellSpacing="0">
                 <thead>
                   <tr>
-                  <th>Product ID</th>
-                  <th>Product Name</th>
-                  <th>Serial Number</th> 
-                  <th>Quantity</th>
-                  <th>Price</th>
-                  <th>Date Added</th>
-                  <th>Action</th>
+                  <th>{strings.productId}</th>
+                  <th>{strings.productName}</th>
+                  <th>{strings.serialNumber}</th> 
+                  <th>{strings.quantity}</th>
+                  <th>{strings.price}</th>
+                  <th>{strings.dateAdded}</th>
+                  <th>{strings.action}</th>
                   </tr>
                 </thead>
               </table>
@@ -113,7 +177,7 @@ export default class Products extends React.Component {
                           <td>{product.quantity}</td>
                           <td>{product.price}</td>
                           <td>{product.dateAdded}</td>
-                          <td><span onClick={() => { this.handleDelete(product.id) }} className="fa fa-trash fa-2x" title="Delete"></span></td>
+                          <td><span onClick={() => { this.handleDelete(product.id) }} className="fa fa-trash fa-2x" title={strings.deleteAction}></span></td>
                         </tr>
                     )
                   })
@@ -126,27 +190,25 @@ export default class Products extends React.Component {
             <div>
             <form onSubmit={this.handleProduct}>
               
-              <button type="button" className="btn-class" onClick={() => this.openModal()}>Add New Product</button>
+              <button type="button" className="btn-class" onClick={() => this.openModal()}>{strings.addAction}</button>
               
               <Modal isOpen={this.state.isModalOpen} onClose={() => this.closeModal()}>
-                <h2>Add New Product</h2>
-                <p>Product name</p>
+                <h2>{strings.addAction}</h2>
+                <p>{strings.productName}</p>
                 <input type="text" id="name" ref="name" required/> 
-                <p>Product serial number</p>
+                <p>{strings.serialNumber}</p>
                 <input type="text" id="serialNumber" ref="serialNumber" required/>
-                <p>Product quantity</p>
+                <p>{strings.quantity}</p>
                 <input type="text" id="quantity" ref="quantity" required/>
-                <p>Product price</p>
+                <p>{strings.price}</p>
                 <input type="text" id="price" ref="price" required/>
-                <p>Product date added</p>
+                <p>{strings.dateAdded}</p>
                 <input type="date" id="dateAdded" ref="dateAdded" required/> <br />
-                <button type="submit" className="btn-class">Add</button>
+                <button type="submit" className="btn-class">{strings.add}</button>
               </Modal>
-              
               </form>
             </div>
           </div>
-            
         );
     }
-}
+  }
